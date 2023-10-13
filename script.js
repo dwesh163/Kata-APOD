497ff8ee2f0635d794582693d23c00d514b67310
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-10-13 09:15:07 +0200

    [add] main css

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let ActivePage = "apod";
+let notActivePage = "settings";
+let quality = "hd";
+
 async function setToday() {
   var now = new Date();
   var day = ("0" + now.getDate()).slice(-2);
@@ -7,6 +11,8 @@ async function setToday() {
 }
 
 async function fetchAPI() {
+  activePageFunction("apod")
+
   const dateInput = document.getElementById("date");
   const API_key = localStorage.getItem("APIKey");
 
@@ -18,7 +24,7 @@ async function fetchAPI() {
       `https://api.nasa.gov/planetary/apod?api_key=${API_key}&date=${dateInput.value}`
     );
     if (response.status != 200) {
-      console.log("set key");
+      activePageFunction("settings")
     } else {
       const JSONObject = await response.json();
       setPage(JSONObject);
@@ -27,6 +33,8 @@ async function fetchAPI() {
     const JSONObject = await response.json();
     setPage(JSONObject);
   }
+
+
 }
 
 function setPage(JSONObject) {
@@ -34,10 +42,14 @@ function setPage(JSONObject) {
   console.log(data);
 
   document.getElementsByClassName("title")[0].innerHTML = data["title"];
-  document.getElementsByClassName("date")[0].innerHTML = data["date"];
+  //document.getElementsByClassName("date")[0].innerHTML = data["date"];
   document.getElementsByClassName("explanation")[0].innerHTML =
     data["explanation"];
-  document.getElementsByClassName("hdurl")[0].src = data["hdurl"];
+  if (quality == "hd") {
+    document.getElementsByClassName("hdurl")[0].src = data["hdurl"];
+  } else {
+    document.getElementsByClassName("hdurl")[0].src = data["url"];
+  }
   if (data["copyright"] != null) {
     document.getElementsByClassName("copyright")[0].innerHTML =
       data["copyright"];
@@ -50,6 +62,7 @@ function setPage(JSONObject) {
 async function setKey() {
   const API_key = document.getElementById("APIKey").value;
   localStorage.setItem("APIKey", API_key);
+  fetchAPI();
 }
 
 function setDay(move) {
@@ -73,19 +86,59 @@ function setDay(move) {
     newDate.setDate(newDate.getDate() + 1);
   }
 
-
   var now = new Date(newDate);
   var day = ("0" + now.getDate()).slice(-2);
   var month = ("0" + (now.getMonth() + 1)).slice(-2);
   var date = now.getFullYear() + "-" + month + "-" + day;
-  document.getElementById("date").value = date
+  document.getElementById("date").value = date;
 
   fetchAPI();
+}
+
+function activePageFunction(active) {
+  if (active == "apod") {
+    ActivePage = "apod";
+    notActivePage = "settings";
+  }
+  if (active == "settings") {
+    ActivePage = "settings";
+    notActivePage = "apod";
+  }
+
+  document.getElementsByClassName(`section-${ActivePage}`)[0].style.visibility =
+    "visible";
+  document.getElementsByClassName(
+    `section-${notActivePage}`
+  )[0].style.visibility = "hidden";
+
+  document.getElementsByClassName(`line-${ActivePage}`)[0].style.visibility =
+    "visible";
+  document.getElementsByClassName(`line-${notActivePage}`)[0].style.visibility =
+    "hidden";
+}
+
+function over(mode) {
+  if (mode != ActivePage) {
+    document.getElementsByClassName(`line-${ActivePage}`)[0].style.visibility =
+      "hidden";
+    document.getElementsByClassName(
+      `line-${notActivePage}`
+    )[0].style.visibility = "visible";
+  }
+}
+
+function out(mode) {
+  if (mode != ActivePage) {
+    document.getElementsByClassName(`line-${ActivePage}`)[0].style.visibility =
+      "visible";
+    document.getElementsByClassName(
+      `line-${notActivePage}`
+    )[0].style.visibility = "hidden";
+  }
 }
 
 setTimeout(() => {
   setToday();
 }, 0.1);
-setTimeout(() => {
-  fetchAPI();
-}, 0.1);
+
+setTimeout(() => {fetchAPI();}, 0.1);
